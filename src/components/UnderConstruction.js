91e0956156e97ebb9871533ed8898dfa2aa275cb
Evan C. Navarro <EvanCNavarro@gmail.com>
2022-08-09 23:03:35 -0400

    feat(UnderConstruction): Banner added
    - Under Construction Banner added to be displayed while portfolio is under development.

---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,11 @@
+export default function UnderConstruction() {
+  return (
+    <div className="h-56 w-full bg-[#FFCB03] border-y-[#ffa303] border-y-4 flex justify-center align-middle items-center space-x-3">
+      <span className="">🚧 </span>
+      <span className="text-offBlack font-extrabold">
+        SITE UNDER CONSTRUCTION
+      </span>
+      <span className="">🚧 </span>
+    </div>
+  );
+}
