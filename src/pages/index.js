58af3b8ed4ce78e7243ece5a1eff5a043f66fd70
Evan C. Navarro <EvanCNavarro@gmail.com>
2022-08-24 21:13:46 -0400

    🔨 fix(Imports)
    - fixed imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,26 @@
-import ContainerBlock from "../components/ContainerBlock";
-import Hero from "../components/Hero";
-import RecentProjects from "../components/RecentProjects";
-import WorkOverview from "../components/WorkOverview";
-import ContactInformation from "../components/ContactInformation";
-import SiteCredits from "../components/SiteCredits";
+import ContainerBlock from "/src/components/0_ContainerBlock";
+import Navbar from "/src/components/1A_Navbar";
+import Banner from "/src/components/1B_Banner";
+// import ProgessBar from "/src/components/1C_ProgressBar";
+import Hero from "/src/components/2_Hero";
+import About from "/src/components/3_About";
+import Projects from "/src/components/4_Projects";
+import Contact from "/src/components/5_Contact";
+import SiteCredits from "/src/components/6A_SiteCredits";
+import Footer from "/src/components/6B_Footer";
 
 export default function Home() {
   return (
     <ContainerBlock>
-
-      {/* <Hero />
-      <RecentProjects /> */}
-      {/* <WorkOverview />
-      <ContactInformation />
-      <SiteCredits /> */}
+      <Navbar />
+      <Banner />
+      {/* <ProgessBar /> */}
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <SiteCredits />
+      <Footer />
     </ContainerBlock>
   );
 }
