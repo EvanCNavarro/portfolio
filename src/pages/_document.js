9f0810ff672a175cd3ab55cc39c9cc7dc1c3fe5e
Evan C. Navarro <EvanCNavarro@gmail.com>
2022-08-24 21:10:11 -0400

    🌐 feat(Google Analytics): added script to site
    - Added 'Google Analytics' to site for catching views and clicks

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,6 +17,23 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital@0;1&family=IBM+Plex+Sans+Condensed:ital@0;1&family=IBM+Plex+Sans:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700&family=IBM+Plex+Serif:ital@0;1&display=swap"
           rel="stylesheet"
         />
+
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <body>
         <Main />
