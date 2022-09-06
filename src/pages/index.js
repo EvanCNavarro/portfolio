d19edc10c97f7225c1e969c935a7ce492fdf85fe
Evan C. Navarro <EvanCNavarro@gmail.com>
2022-09-05 20:23:19 -0400

    🔄 - Disabled screen width Banner
    - the banner that shows the screen width was disabled, since it's only to be seen during development and not when the site is live.

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <ContainerBlock>
       <Navbar />
-      <Banner />
+      {/* <Banner /> */}
       {/* <ProgessBar /> */}
       <Hero />
       <About />
